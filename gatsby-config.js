a1b10a2d77d6b002f3b4a3295e7a0c905c1791dd
Daniel Soares <daniel.soares@me.com>
2020-08-29 20:08:56 -0300

    a fresh new start

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: `Mars Collective`,
+    description: `A collective of creative minds located in Brazil with an international mindset.`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mars Collective`,
+        short_name: `Mars`,
+        start_url: `/`,
+        background_color: `#262626`,
+        theme_color: `#00D1B7`,
+        display: `fullscreen`,
+        icon: `src/images/icon.png`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
+}
