985f8682016d7184304e8471713666840c3a1a03
Daniel Soares <daniel.soares@me.com>
2020-08-29 21:42:03 -0300

    add GlobalStyles and Header

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import Header from '../Header'
+import GlobalStyles from './global'
+import * as S from './styled'
+
 const Layout = ({ children }) => (
-  <>
+  <S.Wrapper>
+    <GlobalStyles />
+    <Header />
     <main>{children}</main>
-  </>
+  </S.Wrapper>
 )
 
 export default Layout
