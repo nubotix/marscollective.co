08dc673706e578fca04d060625b8fd186a34e7af
Daniel Soares <daniel.soares@me.com>
2020-09-06 00:35:08 -0300

    update links

--- src/components/Social/content.js
+++ src/components/Social/content.js
@@ -1,27 +1,27 @@
 const links = [
   {
     label: 'Facebook',
-    url: `https://www.facebook.com`
+    url: `https://www.facebook.com/marscollective.co`
   },
   {
     label: 'Instagram',
-    url: `https://www.instagram.com`
+    url: `https://www.instagram.com/mars.collective/`
   },
   {
     label: 'Behance',
-    url: `https://behance.net`
+    url: `https://www.behance.net/marscollective`
   },
-  {
-    label: 'Medium',
-    url: `https://medium.com`
-  },
+  // {
+  //   label: 'Medium',
+  //   url: `https://medium.com`
+  // },
   {
     label: 'Github',
     url: `https://github.com/marscollective`
   },
   {
     label: 'Whatsapp',
-    url: `https://api.whatsapp.com/send?phone=55`
+    url: `https://api.whatsapp.com/send?phone=5545991328593`
   }
 ]
 
