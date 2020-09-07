1b8527e687f5cf3cf4da2c2fa8e930f3caf4f5df
Daniel Soares <daniel.soares@me.com>
2020-09-07 00:09:16 -0300

    fix anchor link paths

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Location } from '@reach/router'
 
 import LanguageSelector from '../LanguageSelector'
 import * as S from './styled'
@@ -9,65 +10,73 @@ const Nav = () => {
   const [open, setOpen] = useState(false)
 
   return (
-    <nav>
-      <S.StyledBurger open={open} onClick={() => setOpen(!open)}>
-        <div />
-        <div />
-        <div />
-      </S.StyledBurger>
+    <Location>
+      {({ location }) => (
+        <nav>
+          <S.StyledBurger open={open} onClick={() => setOpen(!open)}>
+            <div />
+            <div />
+            <div />
+          </S.StyledBurger>
 
-      <S.List open={open} onClick={() => setOpen(!open)}>
-        <S.ListItem>
-          <S.ItemLink to="/#home" alt={t('Home')} activeClassName="active">
-            <Trans>Home</Trans>
-          </S.ItemLink>
-        </S.ListItem>
-        <S.ListItem>
-          <S.ItemLink
-            to="/#what-we-do"
-            alt={t('What we do')}
-            activeClassName="active"
-          >
-            <Trans>What we do</Trans>
-          </S.ItemLink>
-        </S.ListItem>
-        <S.ListItem>
-          <S.ItemLink
-            to="/#our-work"
-            alt={t('Our work')}
-            activeClassName="active"
-          >
-            <Trans>Our work</Trans>
-          </S.ItemLink>
-        </S.ListItem>
-        <S.ListItem>
-          <S.ItemLink
-            to="/#who-we-are"
-            alt={t('Who we are')}
-            activeClassName="active"
-          >
-            <Trans>Who we are</Trans>
-          </S.ItemLink>
-        </S.ListItem>
-        {/* <S.ListItem>
-          <S.ItemLink to="/#blog" alt={t('Blog')} activeClassName="active">
+          <S.List open={open} onClick={() => setOpen(!open)}>
+            <S.ListItem>
+              <S.ItemLink
+                to={location.pathname + '#home'}
+                alt={t('Home')}
+                activeClassName="active"
+              >
+                <Trans>Home</Trans>
+              </S.ItemLink>
+            </S.ListItem>
+            <S.ListItem>
+              <S.ItemLink
+                to={location.pathname + '#what-we-do'}
+                alt={t('What we do')}
+                activeClassName="active"
+              >
+                <Trans>What we do</Trans>
+              </S.ItemLink>
+            </S.ListItem>
+            <S.ListItem>
+              <S.ItemLink
+                to={location.pathname + '#our-work'}
+                alt={t('Our work')}
+                activeClassName="active"
+              >
+                <Trans>Our work</Trans>
+              </S.ItemLink>
+            </S.ListItem>
+            <S.ListItem>
+              <S.ItemLink
+                to={location.pathname + '#who-we-are'}
+                alt={t('Who we are')}
+                activeClassName="active"
+              >
+                <Trans>Who we are</Trans>
+              </S.ItemLink>
+            </S.ListItem>
+            {/* <S.ListItem>
+          <S.ItemLink to={location.pathname + '#blog'} alt={t('Blog')} activeClassName="active">
             <Trans>Blog</Trans>
           </S.ItemLink>
         </S.ListItem> */}
-        <S.ListItem>
-          <S.ItemLink
-            to="/#contact"
-            alt={t('Contact')}
-            activeClassName="active"
-          >
-            <Trans>Contact</Trans>
-          </S.ItemLink>
-        </S.ListItem>
-        <S.ListItem>
-          <LanguageSelector />
-        </S.ListItem>
-      </S.List>
-    </nav>
+            <S.ListItem>
+              <S.ItemLink
+                to={location.pathname + '#contact'}
+                alt={t('Contact')}
+                activeClassName="active"
+              >
+                <Trans>Contact</Trans>
+              </S.ItemLink>
+            </S.ListItem>
+            <S.ListItem>
+              <LanguageSelector />
+            </S.ListItem>
+          </S.List>
+        </nav>
+      )}
+    </Location>
   )
 }
 
