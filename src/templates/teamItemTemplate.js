4e56c3dfcfb4a9450a1d68a60d10aa219cd47f77
Daniel Soares <daniel.soares@me.com>
2020-09-11 05:18:51 -0300

    :mag: add SEO component

--- src/templates/teamItemTemplate.js
+++ src/templates/teamItemTemplate.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+import { useTranslation } from 'gatsby-plugin-react-i18next'
 
 const TeamItemTemplate = ({ data }) => {
+  const { t } = useTranslation()
   const author = data.markdownRemark.frontmatter
 
   return (
     <Layout>
+      <SEO title={author.name} lang={t('Lang')} />
       <h1>{author.name}</h1>
       <p>{author.position.pt}</p>
     </Layout>
