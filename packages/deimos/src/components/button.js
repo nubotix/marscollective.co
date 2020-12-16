d8383b11b45012711fa0d8736893112996e357d6
Daniel Soares <daniel.soares@me.com>
2020-12-16 03:53:04 -0300

    refactor: Improve button

--- packages/deimos/src/components/button.js
+++ packages/deimos/src/components/button.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const Button = ({ label, primary, small, ...props }) => {
-  const isPrimary = primary
-    ? `bg-mint hover:bg-mint-light text-darker border-transparent`
-    : `bg-transparent text-white hover:text-mint-light border-white hover:border-mint-light`
-  const isSmall = small ? `py-3 px-6` : `py-4 px-8`
-  return (
+const Button = ({ label, link, primary, small, ...props }) => (
+  <a href={link} alt={label}>
     <button
-      className={`${isPrimary} ${isSmall} border-2 font-semibold leading-none rounded-lg transition-colors duration-500`}
+      className={`${
+        primary
+          ? `bg-mint hover:bg-mint-light text-darker border-transparent`
+          : `bg-transparent text-white hover:text-mint-light border-white hover:border-mint-light`
+      } ${
+        small ? `py-3 px-6` : `py-4 px-8`
+      } border-2 font-semibold leading-none rounded-lg transition-colors duration-500`}
       {...props}
     >
       {label}
     </button>
-  )
-}
+  </a>
+)
 
 export default Button
