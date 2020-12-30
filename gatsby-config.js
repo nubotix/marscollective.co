05ecc7e8aab8aab3ede2fa10f2b2f44861c48667
Daniel Soares <daniel.soares@me.com>
2020-12-30 08:58:54 -0300

    chore: lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-const fs = require(`fs`)
-const yaml = require(`js-yaml`)
+const fs = require('fs')
+const yaml = require('js-yaml')
 const metadata = yaml.safeLoad(
-  fs.readFileSync(`content/metadata/index.yaml`, `utf8`)
+  fs.readFileSync('content/metadata/index.yaml', 'utf8')
 )
-const contentPath = `content`
-const favicon = metadata.favicon ? `content/metadata/${metadata.favicon}` : ``
-const supportedLanguages = [`en`, `es`, `pt`]
+const contentPath = 'content'
+const favicon = metadata.favicon ? `content/metadata/${metadata.favicon}` : ''
+const supportedLanguages = ['en', 'es', 'pt']
 
 module.exports = {
   siteMetadata: {
@@ -17,50 +17,50 @@ module.exports = {
     social: metadata.social
   },
   plugins: [
-    `gatsby-plugin-scroll-reveal`,
+    'gatsby-plugin-scroll-reveal',
     {
-      resolve: `@marscollective/gatsby-theme-core`,
+      resolve: '@marscollective/gatsby-theme-core',
       options: { contentPath, favicon, metadata }
     },
     {
-      resolve: `gatsby-plugin-postcss`,
+      resolve: 'gatsby-plugin-postcss',
       options: {
         postCssPlugins: [
-          require(`tailwindcss`)(`${__dirname}/src/theme/tailwind.config.js`),
-          require(`autoprefixer`),
-          require(`postcss-nested`)
+          require('tailwindcss')(`${__dirname}/src/theme/tailwind.config.js`),
+          require('autoprefixer'),
+          require('postcss-nested')
         ]
       }
     },
     {
-      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      resolve: 'gatsby-plugin-react-helmet-canonical-urls',
       options: {
         siteUrl: metadata.siteUrl
       }
     },
     {
-      resolve: `gatsby-plugin-anchor-links`,
+      resolve: 'gatsby-plugin-anchor-links',
       options: {
         offset: -60
       }
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-transformer-remark-frontmatter`,
+            resolve: 'gatsby-transformer-remark-frontmatter',
             options: {
-              whitelist: [`textEN`, `textES`, `textPT`]
+              whitelist: ['textEN', 'textES', 'textPT']
             }
           },
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 1216,
               linkImagesToOriginal: false,
               showCaptions: true,
-              backgroundColor: `transparent`,
+              backgroundColor: 'transparent',
               quality: 100,
               withWebp: true
             }
@@ -69,32 +69,32 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-intl`,
+      resolve: 'gatsby-plugin-intl',
       options: {
         path: `${__dirname}/content/locales`,
         languages: supportedLanguages,
-        defaultLanguage: `en`,
+        defaultLanguage: 'en',
         redirect: true,
         redirectComponent: `${__dirname}/src/components/redirect.js`
       }
     },
     {
-      resolve: `gatsby-plugin-alias-imports`,
+      resolve: 'gatsby-plugin-alias-imports',
       options: {
         alias: {
-          '@assets': `src/assets`,
-          '@collections': `src/cms/collections`,
-          '@components': `src/components`,
-          '@cmsUtils': `src/cms/collections/utils`,
-          '@hooks': `src/hooks`,
-          '@templates': `src/cms/templates`,
-          '@theme': `src/theme`,
-          '@seo': `src/components/seo`
+          '@assets': 'src/assets',
+          '@collections': 'src/cms/collections',
+          '@components': 'src/components',
+          '@cmsUtils': 'src/cms/collections/utils',
+          '@hooks': 'src/hooks',
+          '@templates': 'src/cms/templates',
+          '@theme': 'src/theme',
+          '@seo': 'src/components/seo'
         }
       }
     },
     {
-      resolve: `gatsby-plugin-netlify-cms`,
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         htmlTitle: `${metadata.title}: CMS`,
         htmlFavicon: favicon,
@@ -104,7 +104,7 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-sitemap`,
+      resolve: 'gatsby-plugin-sitemap',
       options: {
         query: `{
           site {
@@ -122,18 +122,18 @@ module.exports = {
           allSitePage.nodes
             .filter(node => {
               const path = node.path
-              if (path.includes(`404`)) {
+              if (path.includes('404')) {
                 return false
               }
-              if (path.includes(`offline-plugin-app-shell-fallback`)) {
+              if (path.includes('offline-plugin-app-shell-fallback')) {
                 return false
               }
-              return supportedLanguages.includes(path.split(`/`)[1])
+              return supportedLanguages.includes(path.split('/')[1])
             })
             .map(node => {
               return {
                 url: `${site.siteMetadata.siteUrl}${node.path}`,
-                changefreq: `weekly`,
+                changefreq: 'weekly',
                 priority: 0.7
               }
             })
